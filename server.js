857910ec8460197096bd7e9b6c2d4b5da1d09d42
Meir Gabay <unfor19@gmail.com>
2021-04-10 15:27:49 +0300

    added inject username to index html

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const express = require('express');
 const favicon = require('serve-favicon');
-
+const { OAuth2Client } = require('google-auth-library');
 
 // Constants
 const index_path = path.join(__dirname + '/index.html');
@@ -14,6 +14,7 @@ const APP_NAME = process.env.APP_NAME;
 const FROM_AUTHOR = process.env.FROM_AUTHOR ? process.env.FROM_AUTHOR : APP_NAME;
 const PORT = process.env.PORT ? process.env.PORT : 8080;
 const HOST = process.env.HOST ? process.env.HOST : '0.0.0.0';
+const CLIENT_ID = process.env.CLIENT_ID ? process.env.CLIENT_ID : '';
 
 
 // App
@@ -38,10 +39,45 @@ const logger = function (req, res, next) {
 app.use(logger);
 
 
+async function verifyToken(token) {
+    const client = new OAuth2Client(CLIENT_ID);
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+            // Or, if multiple clients access the backend:
+            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        });
+        const payload = ticket.getPayload();
+        // const userid = payload['sub'];
+        // If request specified a G Suite domain:
+        // const domain = payload['hd'];
+
+        return payload
+    }
+    return verify().catch(error => {
+        console.log(error);
+        return false;
+    });
+}
+
 // Endpoints
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     var fileStream = read_file();
     var requesting_user = null
+
+    const token = req.headers.authorization.split(" ")[1];
+    if (CLIENT_ID != '') {
+        var verifiedTokenResponse = await verifyToken(token);
+        if (verifiedTokenResponse != false) {
+            console.log(JSON.stringify(verifiedTokenResponse, null, 2));
+            if (verifiedTokenResponse.hasOwnProperty('name')) {
+                requesting_user = verifiedTokenResponse['name']
+            }
+        }
+    }
+
+
     var to_user = requesting_user == null ? "" : requesting_user;
     var parsedStream = fileStream.replace(/APP_NAME/g, APP_NAME).replace(/FROM_AUTHOR/g, FROM_AUTHOR).replace(/TO_USER/g, to_user);
     res.set('Content-Type', 'text/html');
